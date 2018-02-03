65d03618ea71325b5041fd28b9d099a2fa91179b
Gilbert <gilbert.j5@outlook.com>
2018-02-03 11:32:39 -0500

    add react-router

--- dev/scripts/index.js
+++ dev/scripts/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import App from './components/app';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise) (createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App/>
-  </Provider>,
+    <App/>,
   document.getElementById('app'));
